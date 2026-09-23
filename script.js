979ec5c3b29e281d266c221da8a00c8c942477fc
Dhanush-chigurusetti <chigurusettidhanushnagasaimano@gmail.com>
2026-09-23 22:37:08 +0530

    script.js

--- script.js
+++ script.js
@@ -249,7 +249,7 @@ contactForm.addEventListener("submit", (event) => {
 
 
     window.location.href =
-        `mailto:chigurusettidhanush@gmail.com.com?subject=${mailSubject}&body=${mailBody}`;
+        `mailto:chigurusettidhanush@gmail.com?subject=${mailSubject}&body=${mailBody}`;
 
 
     formMessage.textContent =
